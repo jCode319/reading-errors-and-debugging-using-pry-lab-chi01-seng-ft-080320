210e7e103dda990f68c842021d8e2984ac6a9920
Jarrell Jackson <jdjack3@gmail.com>
2020-07-13 15:24:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,10 +12,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-    binding.pry
+  if num = 2
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num = 3
      "HAM HAM HAM IN MY TUMMY"
   end
 end
