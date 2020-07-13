9ee8e3a090e928a00d3ea082c4120a75efbf4883
Jarrell Jackson <jdjack3@gmail.com>
2020-07-13 16:12:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,12 +1,12 @@
 # don't forget to add: require 'pry'
-def get_user_input
-  gets.chomp
-end
-
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
+end
+
+def get_user_input
+  gets.chomp
 end
 
 def selection(num)
