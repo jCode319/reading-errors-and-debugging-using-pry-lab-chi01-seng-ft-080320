557b983b41215b1531102bcd3a9e5680eb9e48e9
Jarrell Jackson <jdjack3@gmail.com>
2020-07-13 15:41:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if string[0] == "s.to_i"
+    10 * "s.to_i" + string
   else
     string
   end
